d58940bd195684ced1498e63fbfb049694af1023
brittany <brittanylgrant00@gmail.com>
2022-08-30 18:12:41 -0400

    update xp spacing

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -2,11 +2,9 @@ const Experience = ({ data }) => (
   <section id="experience" className="min-h-screen flex flex-col justify-center space-y-8 px-10 py-16">
     <h2 className="font-semibold text-4xl">Experience</h2>
     {data.map(({company, location, title, dates, duties, skills}, i) => (
-      <div key={i} className="company-panel max-w-7xl p-8 border-l-8 border-sky-800 bg-zinc-800 font-light">
-        <div className="py-2">
-          <h3 className="text-3xl font-medium">{title}</h3>
-          <p className="text-2xl">at <span className="font-medium">{company}</span>, {location}</p>
-        </div>
+      <div key={i} className="company-panel max-w-7xl p-4 border-l-8 border-sky-800 bg-zinc-800 font-light">
+        <h3 className="text-3xl font-medium">{title}</h3>
+        <p className="pb-2 text-xl">at <span className="font-medium">{company}</span>, {location}</p>
         <p className="text-sm text-zinc-300">{dates}</p>
         <ul className="list-disc pl-8 py-4">
           {duties.map((item, i) => <li key={i}>{item}</li>)}
